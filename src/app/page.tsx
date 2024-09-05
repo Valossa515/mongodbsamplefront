345e4e0b7103f1850f531e58c4559ed4832a376b
Felipe Martins <felipe.mascarenhas@squadra.com.br>
2024-09-05 14:48:31 -0300

    Adicionando Pagina Inicial e navegação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
-import Image from "next/image";
-import Books from "./components/Books";
+import Link from "next/link";
 
-export default function Home() {
+const HomePage: React.FC = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 p-6">
       <section className="max-w-4xl w-full text-center">
         <h1 className="text-4xl font-bold mb-8 text-gray-900 dark:text-gray-100">
           MongoDB CRUD Sample
         </h1>
-        <Books />
+        <p className="text-lg mb-4">Bem-vindo à página inicial!</p>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/books" className="text-blue-500 hover:underline">
+                Ir para Livros
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </section>
     </main>
   );
-}
+};
+
+export default HomePage;
