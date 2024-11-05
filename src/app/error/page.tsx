88e28c2a5a8f617c83bb66f3c928f9240c4ceea9
Felipe Martins <felipe.mascarenhas@squadra.com.br>
2024-11-05 15:02:02 -0300

    Ajustando erros globais

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,14 +1,29 @@
 "use client";
-
-import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import ErrorPage from '../components/ErrorPage';
 
-
 const Error: React.FC = () => {
+  const router = useRouter();
   const searchParams = useSearchParams();
-  const statusCode = searchParams.get('statusCode') || '404';
+  
+  // Captura o statusCode dos parâmetros da URL
+  const statusCode = parseInt(searchParams.get('statusCode') || '404', 10);
 
-  return <ErrorPage statusCode={parseInt(statusCode, 10)} />;
+  useEffect(() => {
+    // Você pode implementar lógica adicional aqui se necessário
+    if (statusCode === 404) {
+      console.log("Página não encontrada.");
+    } else if (statusCode === 500) {
+      console.log("Erro interno do servidor.");
+    } else if (statusCode === 401) {
+      console.log("Usuário não autorizado.");
+    } else if (statusCode === 403) {
+      console.log("Acesso negado.");
+    }
+  }, [router, statusCode]);
+
+  return <ErrorPage statusCode={statusCode} />;
 };
 
 export default Error;
