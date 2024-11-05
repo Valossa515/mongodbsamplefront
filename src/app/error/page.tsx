e5f1b70be425d19495701dff3307eb80299b3607
Felipe Martins <felipe.mascarenhas@squadra.com.br>
2024-11-05 15:05:28 -0300

    Mais ajustes no tratamento de erro global

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,27 +1,21 @@
-"use client";
-import { useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { notFound } from 'next/navigation';
 import ErrorPage from '../components/ErrorPage';
 
-const Error: React.FC = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  
-  // Captura o statusCode dos parâmetros da URL
-  const statusCode = parseInt(searchParams.get('statusCode') || '404', 10);
 
-  useEffect(() => {
-    // Você pode implementar lógica adicional aqui se necessário
-    if (statusCode === 404) {
-      console.log("Página não encontrada.");
-    } else if (statusCode === 500) {
-      console.log("Erro interno do servidor.");
-    } else if (statusCode === 401) {
-      console.log("Usuário não autorizado.");
-    } else if (statusCode === 403) {
-      console.log("Acesso negado.");
-    }
-  }, [router, statusCode]);
+interface ErrorProps {
+  searchParams: {
+    statusCode?: string;
+  };
+}
+
+const Error: React.FC<ErrorProps> = ({ searchParams }) => {
+  // Captura o statusCode dos parâmetros da URL
+  const statusCode = parseInt(searchParams.statusCode || '404', 10);
+
+  // Redireciona ou exibe a página de erro baseada no statusCode
+  if (![401, 403, 404, 500].includes(statusCode)) {
+    return notFound(); // Para códigos de status não tratados, retorna 404
+  }
 
   return <ErrorPage statusCode={statusCode} />;
 };
